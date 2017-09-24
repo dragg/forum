d33dd641b02b83a2fef475d30e0dca2e4b825960
Nikolay Zhidenko <dragg.ko@gmail.com>
2017-09-24 22:54:22 +0300

    Add showing replies on thread page

--- app/Models/Thread.php
+++ app/Models/Thread.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Thread extends Model
 {
-    //
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function replies()
+    {
+        return $this->hasMany(Reply::class);
+    }
 }

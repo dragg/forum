d42304e1d2c3cc46c2bfe7998aab8325eafa28cf
Nikolay Zhidenko <dragg.ko@gmail.com>
2017-10-02 11:34:46 +0300

    Update create thread page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -51,7 +51,14 @@
           <div class="collapse navbar-collapse" id="app-navbar-collapse">
             <!-- Left Side Of Navbar -->
             <ul class="nav navbar-nav">
-              &nbsp;<li><a href="{{ route('threads.index') }}">All Threads</a></li>
+              <li>
+                <a href="{{ route('threads.index') }}">All Threads</a>
+              </li>
+
+              <li>
+                <a href="{{ route('threads.create') }}">New Thread</a>
+              </li>
+
               <li class="dropdown">
                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
                    aria-expanded="false">Channels <span class="caret"></span></a>
